1e090de88a5c7fd121d87bb201cecb7e7a571f91
Roozbehrm <152732283+Roozbehrm@users.noreply.github.com>
2024-05-07 13:22:48 +0330

    Added custom user model
    Created dockerfile and docker-compose file, installed python_decouple to getting environment data from docker-compose file.
    Added custom user model and manager, added profile model, added post_save signal to profile creation

---
+++ core/accounts/admin.py
@@ -0,0 +1,43 @@
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import User, Profile
+# Register your models here.
+
+class CustomUserAdmin(UserAdmin):
+    model = User
+    list_display = ('email', 'is_superuser', 'is_active')
+    list_filter = ('email', 'is_superuser', 'is_active')
+    search_fields = ('email',)
+    ordering = ('email',)
+    fieldsets = (
+        (None, {
+            'fields': (
+                'email', 'password'
+            )
+        }),
+        ('Permission', {
+            'fields': (
+                'is_superuser','is_staff', 'is_active'
+            )
+        }),
+        ('Group Permission', {
+            'fields': (
+                'groups', 'user_permissions'
+            )
+        }),
+        ('Security', {
+            'fields': (
+                'last_login',
+            )
+        }),
+    )
+
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email', 'password1', 'password2', 'is_superuser','is_staff', 'is_active')
+        }),
+    )
+
+admin.site.register(User, CustomUserAdmin)
+admin.site.register(Profile)
